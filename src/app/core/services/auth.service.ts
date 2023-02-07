ce00396a166406d55883b270515d708a52ab718d
Geovana Augusta <geovanaag@hotmail.com>
2023-02-07 18:04:12 -0300

    feat: implementa redirecionamento da rota de login

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable, catchError, map, throwError } from 'rxjs';
 
 @Injectable({
@@ -9,18 +10,29 @@ export class AuthService {
 
   private url: string = 'http://localhost:3000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   public sign(payload: {email: string, password: string}): Observable<any> {
-    return this.http.post(`${this.url}/sign`, payload).pipe(
-      map((data) => {
-        console.log(data);
-
-        return data;
+    return this.http.post<{ token: string} >(`${this.url}/sign`, payload).pipe(
+      map((res) => {
+        localStorage.removeItem('access_token');
+        // Já é string, independendo de JSON.stringify()
+        localStorage.setItem('access_token', res.token);
+        return this.router.navigate(['admin']);
       }),
-      catchError((err) => {
-        return throwError(() => console.log(err));
+      catchError((e) => {
+        if (e.error.message) return throwError(() => e.error.message);
+
+        return throwError(
+          () =>
+            'No momento não estamos conseguindo validar este dados, tente novamente mais tarde!'
+        );
       })
     )
+  }
+
+  public logout() {
+    localStorage.removeItem('access_token');
+    return this.router.navigate([''])
   }
 }
