1113cc4f33bcee2dff07466723ed1aec5c029805
Geovana Augusta <geovanaag@hotmail.com>
2023-02-07 18:18:21 -0300

    feat: constatação que a validação funciona

--- src/app/components/admin/admin-routing.module.ts
+++ src/app/components/admin/admin-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 
 // O path é '' porque na rota pai já atribui admin(auth-routing)
-const routes: Routes = [{
-  path: '', component: HomeComponent
-}];
+const routes: Routes = [
+  {  path: '', component: HomeComponent },
+  // Qualquer outra rota que eu coloque da /admin, a validação ainda sim retorna para / caso não autenticado
+  {  path: 'dashboard', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
